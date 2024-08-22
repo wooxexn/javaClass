54bd1d838844dbc0d2b0b945493f80601593fc5c
전우선 <jeonwooseon@naver.com>
2024-08-22 15:03:27 +0900

    20240822

---
+++ day14/인터페이스/라이브러리/MyLibMain.java
@@ -0,0 +1,13 @@
+package day14.인터페이스.라이브러리;
+
+public class MyLibMain {
+
+	public static void main(String[] args) {
+		
+		
+		MyLib lib = new MyLib();
+		lib.yourProgramRun(new A());
+
+	}
+
+}
---
+++ day14/인터페이스/라이브러리/MyRunnable.java
@@ -0,0 +1,8 @@
+package day14.인터페이스.라이브러리;
+
+public interface MyRunnable {
+	
+	public void run();
+	
+
+}
